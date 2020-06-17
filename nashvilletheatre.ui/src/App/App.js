63f700f16523af8dca61a0ec9c4bcbf7a90adb9e
Denise Baker <denviol@yahoo.com>
2020-06-16 20:03:53 -0500

    wip

--- nashvilletheatre.ui/src/App/App.js
+++ nashvilletheatre.ui/src/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../components/shared/navbar/Navbar';
+import Navbar from '../components/shared/navbar/navbar';
 import './App.scss';
 
 function App() {
---
+++ nashvilletheatre.ui/src/helpers/data/showData.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { baseUrl } from '../../apiKeys.json';
+
+const getAllShows = () => new Promise((resolve, reject) => {
+    axios.get(`${baseUrl}/api/show`).then((result) => {
+        const allShows = result.data;
+        resolve(allShows);
+    }).catch((error) => reject(error));
+});
+
+export default { getAllShows };
+ 
