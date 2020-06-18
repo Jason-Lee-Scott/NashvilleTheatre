30e83f4d2b6812fd5123c6ea32028ca65f2aae68
Kelsey Creel <kelseycreel@gmail.com>
2020-06-17 19:43:27 -0500

    ui for all categories

--- nashvilletheatre.ui/src/App/App.js
+++ nashvilletheatre.ui/src/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../components/shared/navbar/Navbar';
+import Navbar from '../components/shared/navbar/navbar';
 import Footer from '../components/shared/footer/Footer';
 import './App.scss';
 
