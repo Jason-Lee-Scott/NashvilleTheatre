2c5b08b71a2eabb383ca33cd991008d83c8c7c7c
Jacob <wittenberg.jacob@gmail.com>
2020-06-20 13:19:45 -0500

    added firebase info. installed nuget packages for authentication/authorization

--- NashvilleTheatre/Startup.cs
+++ NashvilleTheatre/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using NashvilleTheatre.DataAccess;
 
 namespace NashvilleTheatre
@@ -33,6 +35,22 @@ namespace NashvilleTheatre
                     builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin())
             );
 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                    {
+                        options.IncludeErrorDetails = true;
+                        options.Authority = authSettings["Authority"];
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            ValidIssuer = authSettings["Issuer"],
+                            ValidateAudience = true,
+                            ValidAudience = authSettings["Audience"],
+                            ValidateLifetime = true
+                        };
+                    }
+                );
+
             services.AddTransient<CategoryRepository>();
             services.AddTransient<OrderRepository>();
             services.AddTransient<ShowRepository>();
@@ -53,6 +71,8 @@ namespace NashvilleTheatre
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseCors("ItsAllGood");
 
