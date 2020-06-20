52d31944a71ec25a27bb53c87638f681d6b0c651
EvanG <evangdesigns@gmail.com>
2020-06-20 13:29:55 -0500

    ticket shape is complete

--- nashvilletheatre.ui/src/components/shared/Show/Show.js
+++ nashvilletheatre.ui/src/components/shared/Show/Show.js
@@ -1,33 +1,42 @@
 import React from 'react';
 import moment from 'moment';
 import './Show.scss';
+import { Link } from 'react-router-dom';
 
 
 class Show extends React.Component {
 
-    render() {
-        const { show } = this.props;
-        return (
-            <div className="card col-3 showcard">
-                <div className="align-up row">
-                <div className="show-margin col-1"></div><div className="clip-circle-left col-2"></div>
-                <img src={show.showImageUrl} className="card-img-top show-image col-12" alt="..."/>
-                <div className="show-margin col-1"></div><div className="clip-circle-right col-2"></div>
-                </div>
-                <div className="card-body showcard">
-                    <h5 className="card-title showname">{show.showName}</h5>
-                    <div className="d-flex justify-content-between">
-                    <p className="showdate">{moment(show.showDate).format('L')}</p>
-                    <p className="showtime">{moment(show.showTime).format('LT')}</p>
+	render() {
+		const { show } = this.props;
+		return (
 
-                    </div>
-                    <p className="card-text venue">{show.venueName}</p>
-                    
-                </div>
-                
-            </div>
-        );
-    }
+			<div className="show-card">
+				<div className="ticket-image">
+					<svg height="0" width="0">
+						<defs>
+							<clipPath id="ticket-clip">
+								<path d="M320,53.2V0H0V53.2C12.8,53.2,23.2,63,23.2,75S12.8,96.8,0,96.8V150H320V96.8c-12.8,0-23.2-9.8-23.2-21.8S307.2,53.2,320,53.2Z"/>
+							</clipPath>
+						</defs>
+					</svg>
+					<Link to={`/show/${show.showId}`}>
+					<img src={show.showImageUrl} alt={show.showName}/>
+					</Link>
+				</div>
+				<div className="show-info">
+					<h5 className="show-name">{show.showName}</h5>
+						<div className="red-dash"></div>
+						<div className="d-flex justify-content-between">
+							<p className="show-date">{moment(show.showDate).format('L')}</p>
+							<p className="show-time">{moment(show.showTime).format('LT')}</p>
+						</div>
+					<p className="venue">{show.venueName}</p>
+					<Link className="view-info" to={`/show/${show.showId}`}><h4>View Info</h4></Link>
+				</div>
+			</div>
+
+		);
+	}
 }
 
 export default Show;
