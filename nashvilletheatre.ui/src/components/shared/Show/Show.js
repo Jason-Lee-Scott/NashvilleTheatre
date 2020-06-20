8963398ef5553f6587fb1b8c861db988c366aab5
Denise Baker <denviol@yahoo.com>
2020-06-20 09:48:27 -0500

    styling wip

--- nashvilletheatre.ui/src/components/shared/Show/Show.js
+++ nashvilletheatre.ui/src/components/shared/Show/Show.js
@@ -8,14 +8,20 @@ class Show extends React.Component {
     render() {
         const { show } = this.props;
         return (
-            <div className="card d-inline-flex row col-3">
-                <div><div className="clip-circle-left"></div></div>
-                <img src={show.showImageUrl} className="card-img-top" alt="..."/>
-                <div className="card-body">
-                    <h5 className="card-title">{show.showName}</h5>
-                    <p>{moment(show.showDate).format('L')}</p>
-                    <p>{moment(show.showTime).format('LT')}</p>
-                    <p className="card-text">{show.venueName}</p>
+            <div className="card col-3 showcard">
+                <div className="align-up row">
+                <div className="show-margin col-1"></div><div className="clip-circle-left col-2"></div>
+                <img src={show.showImageUrl} className="card-img-top show-image col-12" alt="..."/>
+                <div className="show-margin col-1"></div><div className="clip-circle-right col-2"></div>
+                </div>
+                <div className="card-body showcard">
+                    <h5 className="card-title showname">{show.showName}</h5>
+                    <div className="d-flex justify-content-between">
+                    <p className="showdate">{moment(show.showDate).format('L')}</p>
+                    <p className="showtime">{moment(show.showTime).format('LT')}</p>
+
+                    </div>
+                    <p className="card-text venue">{show.venueName}</p>
                     
                 </div>
                 
