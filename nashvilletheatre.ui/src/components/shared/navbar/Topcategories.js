14d43c0fcf9dc0adab0980ec16814e734dbf7d44
Kelsey Creel <kelseycreel@gmail.com>
2020-06-28 09:09:51 -0500

    WIP

--- nashvilletheatre.ui/src/components/shared/navbar/Topcategories.js
+++ nashvilletheatre.ui/src/components/shared/navbar/Topcategories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getTopCategories } from '../../../helpers/data/categoryData';
+import { getTopCategories } from '../../../helpers/data/CategoryData';
 import './navbar.scss';
 import Category from '../Category/Category';
 
